195faf2e234491720fb77302f6be18613fe6fb0c
REN620813 <43432777+REN620813@users.noreply.github.com>
2018-09-20 16:40:28 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[256];
+    char str[256];"Hello world!"
     fgets(str, sizeof(str), stdin);
-    printf("%s\n", "XXXXX");
+    printf("Hello world!");
 
     return 0;
 }
